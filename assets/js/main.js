6d47045d28623a6e87a754350a61e71fb1d5c6af
Samuel Aliyari <samuelaliyari@outlook.de>
2023-10-25 23:44:09 +0200

    Bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
 console.log("test")
-
+const modal = document.body.querySelector(".modal");
+const homeName = document.body.querySelector(".modal input:nth-of-type(1)");
+const awayName = document.body.querySelector(".modal input:nth-of-type(2)");
 const scoreHome = document.body.querySelector("#scoreHome");
 const scoreAway = document.body.querySelector("#scoreAway");
+const homeTitle = document.body.querySelector("#homeName");
+const awayTitle = document.body.querySelector("#awayName");
+
+
+const showModal = () => modal.style.opacity = "0.9";
+const saveNames = () => {
+    homeTitle.textContent = homeName.value;
+    awayTitle.textContent = awayName.value;
+    modal.style.opacity = "0";
+}
 
 const scoreHomeChange = (points) =>scoreHome.textContent = Number(scoreHome.textContent) + points ;
 const scoreAwayChange = (points) =>scoreAway.textContent = Number(scoreAway.textContent) + points ;
